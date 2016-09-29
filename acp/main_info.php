4a22f0beccca0f37f7118997c8a9ee0603192a30
AustinMaddox <austin@maddoxbox.com>
2016-09-28 20:09:37 -0600

    Releasing a 0.1.2 version

--- acp/main_info.php
+++ acp/main_info.php
@@ -16,7 +16,7 @@ class main_info
         return [
             'filename' => '\AustinMaddox\s3\acp\main_module',
             'title'    => 'ACP_S3_TITLE',
-            'version'  => '0.1.1',
+            'version'  => '0.1.2',
             'modes'    => [
                 'settings' => [
                     'title' => 'ACP_S3',
